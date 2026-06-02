adaea3d62957b9dd65c41dbebbc43e0158503217
Umesh Kute <umesh.k@ad.valuenable.in>
2026-06-02 10:18:11 +0530

    UL automation framework completed

---
+++ src/test/java/com/valuenable/constants/ExecutionStatus.java
@@ -0,0 +1,16 @@
+package com.valuenable.constants;
+
+public enum ExecutionStatus {
+
+	PASS,
+
+	FAIL,
+
+	POLICY_NOT_FOUND,
+
+	SNAPSHOT_NOT_FOUND,
+
+	FUTURE_PERFORMANCE_NOT_FOUND,
+
+	APPLICATION_ERROR
+}
